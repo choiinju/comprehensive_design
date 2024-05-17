a11797748eff4cc2e741b3f7eacd32af920100ed
kw0n0 <alwayskw0n0@gmail.com>
2024-05-18 00:59:18 +0900

    feat: 로컬에 저장된 파일을 불러오는 로직 삭제 및 음성 데이터 변환로직 추가

--- server/intoeng/audioapp/transcribe.py
+++ server/intoeng/audioapp/transcribe.py
@@ -1,11 +1,9 @@
 import pickle
-import soundfile as sf
 
-def transcribe(audio_path):
-    with open('/Users/ryan/Desktop/intoeng/comprehensive_design/server/intoeng/audioapp/whisper_small_0428.pkl', 'rb') as f:
-        asr = pickle.load(f)       
+with open('/Users/ryan/Desktop/intoeng/comprehensive_design/server/intoeng/audioapp/whisper_small_0428.pkl', 'rb') as f:
+        asr = pickle.load(f)     
 
-    audio_data, _ = sf.read(audio_path)  # 오디오 데이터만 추출 
+def transcribe(audio_data):
     # 음성 데이터를 파이프라인에 전달
     transcription = asr(audio_data)
     return transcription['text']  # 텍스트 추출
--- server/intoeng/audioapp/views.py
+++ server/intoeng/audioapp/views.py
@@ -6,6 +6,10 @@ from rest_framework import status
 from rest_framework import generics
 from audioapp import transcribe
 
+import numpy as np
+import io
+from scipy.io.wavfile import read
+
 from .models import AudioFile
 from .serializers import AudioFileSerializer
 
@@ -15,11 +19,18 @@ class AudioUploadView(APIView):
         if serializer.is_valid():
             serializer.save()
 
-            print(request.data)
+            audio_bytes= request.data['audio'].file.getvalue()        
+            _, data = read(io.BytesIO(audio_bytes))
 
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")        
-            result = transcribe.transcribe("/Users/ryan/Desktop/intoeng/comprehensive_design/server/intoeng/audioapp/audio.wav")
-            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
+            # # data가 스테레오인 경우(2채널), 모노(1채널)로 변환
+            if len(data.shape) > 1:
+                data = np.mean(data, axis=1)
+
+            # 데이터 정규화
+            data = data / np.max(np.abs(data),axis=0)      
+            
+            result = transcribe.transcribe(data)
+
             return Response(result, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
