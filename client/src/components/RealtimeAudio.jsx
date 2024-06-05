8a66c65b348b4aeb6208061dbec5bef86ebfaf1d
RyanKw0n <alwayskyu2920@gmail.com>
2024-06-05 13:56:26 +0900

    feat: 테스트 화면 디자인

--- client/src/components/RealtimeAudio.jsx
+++ client/src/components/RealtimeAudio.jsx
@@ -1,11 +1,21 @@
 import React, { useState, useEffect, useRef } from 'react';
+import styles from './realtime_audio.module.scss';
+import start from '../images/start.svg';
+import stop from '../images/stop.svg';
+import copy from '../images/copy.svg';
+import { Link } from 'react-router-dom';
+import Lottie from 'lottie-react';
+import loading from '../images/loading.json';
 
 const RealtimeAudio = () => {
   const [recording, setRecording] = useState(false);
   const mediaRecorderRef = useRef(null);
   const chunksRef = useRef([]);
   const socketRef = useRef(null);
-  const [text, setText] = useState('');
+  const [text, setText] = useState([]);
+  const [seconds, setSeconds] = useState(0);
+  const [isActive, setIsActive] = useState(false);
+  const messagesEndRef = useRef(null);
 
   useEffect(() => {
     socketRef.current = new WebSocket('ws://localhost:8080/ws/intoeng/');
@@ -20,7 +30,7 @@ const RealtimeAudio = () => {
     };
     socketRef.current.onmessage = (event) => {
       let msg = JSON.parse(event.data).message;
-      setText((text) => text + msg);
+      setText((text) => [...text, msg]);
     };
 
     return () => {
@@ -29,6 +39,24 @@ const RealtimeAudio = () => {
       }
     };
   }, []);
+
+  useEffect(() => {
+    let interval = null;
+
+    if (isActive) {
+      interval = setInterval(() => {
+        setSeconds((seconds) => seconds + 1);
+      }, 1000);
+    } else if (!isActive && seconds !== 0) {
+      clearInterval(interval);
+    }
+
+    return () => clearInterval(interval);
+  }, [isActive, seconds]);
+
+  useEffect(() => {
+    scrollToBottom();
+  }, [text]);
 
   function checkVolume(st) {
     const audioContext = new (window.AudioContext ||
@@ -115,15 +143,119 @@ const RealtimeAudio = () => {
     }
   };
 
+  const copyContent = () => {
+    navigator.clipboard.writeText(text.join('\n'));
+  };
+
+  const formatTime = (seconds) => {
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = seconds % 60;
+    return `${String(minutes).padStart(2, '0')}:${String(
+      remainingSeconds
+    ).padStart(2, '0')}`;
+  };
+
+  const scrollToBottom = () => {
+    messagesEndRef.current?.scrollIntoView({ behavior: 'smooth' });
+  };
+
   return (
     <div>
-      <button onClick={startRecording} disabled={recording}>
-        녹음 시작
-      </button>
-      <button onClick={stopRecording} disabled={!recording}>
-        녹음 중지
-      </button>
-      {text}
+      <div
+        style={{
+          borderBottom: '3px solid #EBEBEB',
+          display: 'flex',
+          justifyContent: 'space-between',
+          position: 'fixed',
+          top: 0,
+          left: 0,
+          width: '100%',
+          backgroundColor: 'white',
+        }}
+      >
+        <Link className={`${styles.logo}`} to="/">
+          INTOENG
+        </Link>
+
+        <div style={{ display: 'flex', alignItems: 'center' }}>
+          {recording ? (
+            <img
+              src={stop}
+              width={'40px'}
+              height={'40px'}
+              onClick={() => {
+                stopRecording();
+                setIsActive(false);
+              }}
+            />
+          ) : (
+            <img
+              src={start}
+              width={'40px'}
+              height={'40px'}
+              onClick={() => {
+                startRecording();
+                setIsActive(true);
+              }}
+            />
+          )}
+          <div style={{ marginRight: '20px' }} />
+          <div className={`${styles.timer}`}>{formatTime(seconds)}</div>
+          <div style={{ marginRight: '20px' }} />
+        </div>
+      </div>
+      <div style={{ marginTop: '100px' }} />
+      <div className={`${styles.start_notice}`}>
+        <div>
+          {`안녕하세요.\n저는 당신의 인도 영어 이해를 도와줄 도우미,`}
+          <span className={`${styles.orange}`}> INTOENG</span>
+          {`입니다. \n녹음 방법과 주의사항을 안내해 드리겠습니다.\n\n`}
+          <span className={`${styles.bigger}`}>녹음 방법</span>
+          {`\n1) 우측 상단의`}
+
+          <span className={`${styles.red}`}> 빨간 동그라미 버튼</span>
+          {`을 클릭하면 녹음이 시작됩니다.\n2) 녹음을 중단하고 싶으시면, 동일한 위치의`}
+          <span className={`${styles.black}`}> 멈춤 버튼</span>
+          {`을 눌러주세요.\n\n`}
+          <span className={`${styles.bigger}`}>주의사항</span>
+          {`\n녹음 내역은 저장되지 않습니다.`}
+        </div>
+      </div>
+      {text.length !== 0 && (
+        <div className={`${styles.res_container}`}>
+          <div className={`${styles.response}`}>
+            {text.join('\n')}
+            <div style={{ marginTop: '10px' }} />
+            {!recording && text.length !== 0 && (
+              <div className={`${styles.copy}`}>
+                <img
+                  src={copy}
+                  width={'17px'}
+                  height={'17px'}
+                  onClick={startRecording}
+                />
+                <div onClick={copyContent}>간편 복사하기</div>
+              </div>
+            )}
+          </div>
+
+          {recording && (
+            <Lottie
+              style={{ width: '200px', height: '100px' }}
+              animationData={loading}
+            />
+          )}
+        </div>
+      )}
+      {!recording && text.length !== 0 && (
+        <div>
+          <div className={`${styles.start_notice}`}>
+            {`스크립트 출력이 끝났습니다.\n이용해주셔서 감사합니다.`}
+          </div>
+          <div style={{ marginTop: '30px' }} />
+        </div>
+      )}
+      <div ref={messagesEndRef} />
     </div>
   );
 };
--- client/src/pages/Test.js
+++ client/src/pages/Test.js
@@ -1,11 +1,9 @@
 import RealtimeAudio from '../components/RealtimeAudio';
-import { Link } from 'react-router-dom';
 
 function Test() {
   return (
     <div>
       <RealtimeAudio />
-      <Link to="/">홈으로 </Link>
     </div>
   );
 }
